8c6680b6515f9ab22e87f70667726d2b991e1670
cycjiuk8704 <cycjiuk8704@gmail.com>
2021-05-13 19:08:59 +0300

    created repository

---
+++ src/lesson1/MyData.java
@@ -0,0 +1,17 @@
+package lesson1;
+
+public class MyData {
+    private final char myCharData;
+
+    public MyData(char myCharData) {
+        this.myCharData = myCharData;
+    }
+
+    public char getMyCharData() {
+        return myCharData;
+    }
+
+    public void printToConsoleMyCharData() {
+        System.out.println(myCharData);
+    }
+}
---
+++ src/lesson2/Main.java
@@ -0,0 +1,48 @@
+package lesson2;
+
+import lesson1.MyData;
+
+public class Main {
+    public static void main(String[] args) {
+
+        char[] primitivesArray = new char[]{
+                'a', 'b', '1', '5', 'f', 'd', 'c', '2'
+        };
+
+        String[] referenceArray = new String[]{
+                "a", "b", "1", "5", "f", "d", "c", "2"
+        };
+
+        MyData[] myDataArray = new MyData[8];
+        myDataArray[0] = new MyData('a');
+        myDataArray[1] = new MyData('b');
+        myDataArray[2] = new MyData('1');
+        myDataArray[3] = new MyData('5');
+        myDataArray[4] = new MyData('f');
+        myDataArray[5] = new MyData('d');
+        myDataArray[6] = new MyData('c');
+        myDataArray[7] = new MyData('2');
+
+        long startTime = System.nanoTime();
+        for (char c : primitivesArray) {
+            if (c == 'd') {
+                System.out.println(System.nanoTime() - startTime);
+            }
+        }
+
+        startTime = System.nanoTime();
+        for (String s : referenceArray) {
+            if (s.equals("d")) {
+                System.out.println(System.nanoTime() - startTime);
+            }
+        }
+
+        startTime = System.nanoTime();
+        for (MyData myData : myDataArray) {
+            if (myData.getMyCharData() == 'd') {
+                System.out.println(System.nanoTime() - startTime);
+            }
+        }
+    }
+
+}
